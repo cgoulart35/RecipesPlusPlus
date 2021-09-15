59c836ac984065774992d751b929b3f47d47bcf8
Christopher Goulart <cgoulart35@gmail.com>
2021-09-14 21:30:37 -0400

    Refactored app structure

--- RecipesPlusPlusApi/main.py
+++ RecipesPlusPlusApi/main.py
@@ -2,30 +2,36 @@
 import json
 import pyrebase
 import logging
+import importlib.util
 import pathlib
 from configparser import ConfigParser
-from flask import Flask
-from flask_restful import Api
-
-from endpoints.ingredients import Ingredients
-from endpoints.recipes import Recipes
-from endpoints.users import Users
+from flask import Flask, request
+from flask_restful import Api, Resource
 #endregion
 
-# get parent directory
+# get parent directory and dependencies
 parentDir = str(pathlib.Path(__file__).parent.parent.absolute())
 parentDir = parentDir.replace("\\",'/')
 
+spec = importlib.util.spec_from_file_location('shared', parentDir + '/Shared/functions.py')
+functions = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(functions)
+
+spec = importlib.util.spec_from_file_location('shared', parentDir + '/Shared/queries.py')
+queries = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(queries)
+
 # get configuration variables
-config = ConfigParser()
-config.read('RecipesPlusPlusApi/config.ini')
+apiConfig = ConfigParser()
+apiConfig.read('RecipesPlusPlusApi/api.ini')
+sharedConfig = functions.buildSharedConfig(parentDir)
 
 # initialize firebase and database
-firebaseConfig = json.loads(config['properties']['firebaseConfigJson'])
+firebaseConfig = json.loads(sharedConfig['properties']['firebaseConfigJson'])
 firebase = pyrebase.initialize_app(firebaseConfig)
 db = firebase.database()
 auth = firebase.auth()
-user = auth.sign_in_with_email_and_password(config['properties']['firebaseAuthEmail'], config['properties']['firebaseAuthPassword'])
+user = auth.sign_in_with_email_and_password(sharedConfig['properties']['firebaseAuthEmail'], sharedConfig['properties']['firebaseAuthPassword'])
 token = user['idToken']
 
 # create and configure logger
@@ -35,6 +41,46 @@ logging.basicConfig(filename = parentDir + '/RecipesPlusPlusApi.log', level = lo
 # Flask REST API
 app = Flask(__name__)
 api = Api(app)
+
+class Ingredients(Resource):
+    def get(self, id=None):
+        if not id:
+            # get all ingredients
+            return 1
+        else:
+            # get specific ingredient
+            return id
+    def post(self):
+        return
+    def delete(self, id):
+        return
+
+class Recipes(Resource):
+    def get(self, id=None):
+        if not id:
+            # get all recipes
+            return 1
+        else:
+            # get specific recipe
+            return id
+    def post(self):
+        return
+    def delete(self, id):
+        return
+
+class Users(Resource):
+    def get(self, id=None):
+        if not id:
+            # get all users
+            return 1
+        else:
+            # get specific user
+            return id
+    def post(self):
+        return
+    def delete(self, id):
+        return
+
 api.add_resource(Ingredients, '/RecipesPlusPlus/ingredients/', '/RecipesPlusPlus/ingredients/<int:id>/')
 api.add_resource(Recipes, '/RecipesPlusPlus/recipes/', '/RecipesPlusPlus/recipes/<int:id>/')
 api.add_resource(Users, '/RecipesPlusPlus/users/', '/RecipesPlusPlus/users/<int:id>/')
---
+++ Shared/functions.py
@@ -0,0 +1,12 @@
+import configparser
+import json
+import pyrebase
+
+def buildSharedConfig(parentDir):
+    sharedConfig = configparser.ConfigParser()
+    sharedConfig.read(parentDir + '/Shared/shared.ini')
+    return sharedConfig
+
+def buildFirebase(sharedConfig):
+    firebaseConfig = json.loads(sharedConfig['shared']['firebaseConfigJson'])
+    return pyrebase.initialize_app(firebaseConfig)
